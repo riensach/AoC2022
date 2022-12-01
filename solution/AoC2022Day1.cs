3553600e872dd79c52d6a256ac89427fb0a7534f
Rien Sach <rien.sach@aptem.co.uk>
2022-12-01 19:21:52 +0000

    A little neater

--- solution/AoC2022Day1.cs
+++ solution/AoC2022Day1.cs
@@ -10,88 +10,50 @@ namespace AoC2022.solution
 
         public AoCDay1(int selectedPart, string input)
         {
-            StreamWriter logFile = File.CreateText("day1.log");
-            Trace.Listeners.Add(new TextWriterTraceListener(logFile));
-            Trace.AutoFlush = true;
-            Trace.WriteLine("Starting Day1 Log");
-            Trace.WriteLine(string.Format("Started {0}", DateTime.Now.ToString()));
-
 
             string[] lines = input.Split(
                 new string[] { Environment.NewLine },
                 StringSplitOptions.None
             );
 
-            if(selectedPart == 1)
+            List<int> elves = new List<int>();
+
+            int calories = 0;
+            foreach (string line in lines)
             {
-                partA(lines);
-            } else
-            {
-                partB(lines);
+                if (line == null || line == "")
+                {
+                    elves.Add(calories);
+                    calories = 0;
+                    continue;
+                }
+                calories += Int32.Parse(line);
+
             }
-           
+            elves.Sort();
+            elves.Reverse();
 
-
+            output = partA(elves);
+            output += partB(elves);
             
         }
 
         public string output;
 
-        private string partA(string[] lines) {
-            int value = 0;
-
-            int elf = 0;
-            foreach (string line in lines)
-            {
-                //Console.Write(line+"\n");
-                if (line == null || line == "")
-                {
-                    elf++;
-                    continue;
-                }
-                
-                
-            }
-
-            int[] elves = new int[elf+1];
-            Console.Write(elf);
-            Trace.WriteLine("Starting Day1 Log" + elf);
-            elf = 0;
-            foreach (string line in lines)
-            {
-                if (line == null || line == "")
-                {
-                    elf++;
-                    continue;
-                }
-
-                //Console.Write(elf);
-                elves[elf] += Int32.Parse(line);
-                
-
-            }
-            Array.Sort(elves);
-            Array.Reverse(elves);
-
-            value = elves[0] + elves[1] + elves[2];
-            output = "Onput value: " + value;
+        private string partA(List<int> calories) {
+            int value = 0;     
+            value = calories[0];
+            output = "Part A value: " + value;
             return output;
         }
 
-
-        private string partB(string[] lines)
+        private string partB(List<int> calories)
         {
             int value = 0;
-
-            foreach (string line in lines)
-            {
-                value += Int32.Parse(line);
-            }
-
-            output = "Onput value: " + value;
+            value = calories[0] + calories[1] + calories[2];
+            output = "\nPart B value: " + value;
             return output;
         }
-
 
     }
 }
