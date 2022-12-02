0f7262c8a7bbc19d1ca1aa1e0c773856ccd4f26f
Rien Sach <rien.sach@aptem.co.uk>
2022-12-02 05:24:36 +0000

    Day 2, first version

--- Launch.cs
+++ Launch.cs
@@ -16,7 +16,7 @@ namespace AdventofCode2022
             Console.WriteLine("------------------------\n");
 
 
-            int daySelection = 1;
+            int daySelection = 2;
             int partSelection = 1;
 
             DaySelection Advent = new DaySelection(daySelection, partSelection);
--- solution/AoC2022Day2.cs
+++ solution/AoC2022Day2.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Diagnostics;
+using System.Threading.Tasks.Sources;
 
 namespace AoC2022.solution
 {
@@ -9,11 +10,104 @@ namespace AoC2022.solution
 
         public AoCDay2(int selectedPart, string input)
         {
-            StreamWriter logFile = File.CreateText("day2.log");
-            Trace.Listeners.Add(new TextWriterTraceListener(logFile));
-            Trace.AutoFlush = true;
-            Trace.WriteLine("Starting Calculator Log");
-            Trace.WriteLine(string.Format("Started {0}", DateTime.Now.ToString()));
+
+            string[] lines = input.Split(
+                new string[] { Environment.NewLine },
+                StringSplitOptions.None
+            );
+            int score = 0;
+
+            foreach (string line in lines)
+            {
+                string[] moves = line.Split(' ');
+
+                if (moves[0].Equals("A") && moves[1].Equals("X")) // Rock vs Rock
+                {
+                    score = score + 4;
+                }
+                else if(moves[0].Equals("A") && moves[1].Equals("Y")) // Rock vs Paper
+                {
+                    score = score + 8;
+                }
+                else if(moves[0].Equals("A") && moves[1].Equals("Z")) // Rock vs Sciccors 
+                {
+                    score = score + 3;
+                }
+                else if(moves[0].Equals("B") && moves[1].Equals("X")) // Paper vs Rock
+                {
+                    score = score + 1;
+                }
+                else if(moves[0].Equals("B") && moves[1].Equals("Y")) // Paper vs Paper
+                {
+                    score = score + 5;
+                }
+                else if(moves[0].Equals("B") && moves[1].Equals("Z")) // Paper vs Sciccors 
+                {
+                    score = score + 9;
+                }
+                else if(moves[0].Equals("C") && moves[1].Equals("X")) // Sciccors vs Rock
+                {
+                    score = score + 7;
+                }
+                else if(moves[0].Equals("C") && moves[1].Equals("Y")) // Sciccors vs Paper
+                {
+                    score = score + 2;
+                }
+                else if (moves[0].Equals("C") && moves[1].Equals("Z")) // Sciccors vs Sciccors 
+                {
+                    score = score + 6;
+                }                
+
+            }
+
+            output = "Part A: " + score;
+
+            score = 0;
+            foreach (string line in lines)
+            {
+                string[] moves = line.Split(' ');
+
+                if (moves[0].Equals("A") && moves[1].Equals("X")) // Rock vs Rock (must lose)
+                {
+                    score = score + 3;
+                }
+                else if (moves[0].Equals("A") && moves[1].Equals("Y")) // Rock vs Paper (must draw)
+                {
+                    score = score + 4;
+                }
+                else if (moves[0].Equals("A") && moves[1].Equals("Z")) // Rock vs Sciccors (must win)
+                {
+                    score = score + 8;
+                }
+                else if (moves[0].Equals("B") && moves[1].Equals("X")) // Paper vs Rock (must lose)
+                {
+                    score = score + 1;
+                }
+                else if (moves[0].Equals("B") && moves[1].Equals("Y")) // Paper vs Paper (must draw)
+                {
+                    score = score + 5;
+                }
+                else if (moves[0].Equals("B") && moves[1].Equals("Z")) // Paper vs Sciccors (must win) 
+                {
+                    score = score + 9;
+                }
+                else if (moves[0].Equals("C") && moves[1].Equals("X")) // Sciccors vs Rock (must lose)
+                {
+                    score = score + 2;
+                }
+                else if (moves[0].Equals("C") && moves[1].Equals("Y")) // Sciccors vs Paper (must draw)
+                {
+                    score = score + 6;
+                }
+                else if (moves[0].Equals("C") && moves[1].Equals("Z")) // Sciccors vs Sciccors (must win)
+                {
+                    score = score + 7;
+                }
+
+            }
+
+            output += "\nPart B: " + score;
+
         }
 
         public string output;
