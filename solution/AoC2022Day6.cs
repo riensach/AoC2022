b4d786ad392dad90e2f4bfac51aa5ba96e38e6c0
Rien Sach <rien.sach@aptem.co.uk>
2022-12-06 05:19:36 +0000

    Day 6 - quite fun!

--- solution/AoC2022Day6.cs
+++ solution/AoC2022Day6.cs
@@ -9,11 +9,43 @@ namespace AoC2022.solution
 
         public AoCDay6(int selectedPart, string input)
         {
-            StreamWriter logFile = File.CreateText("day6.log");
-            Trace.Listeners.Add(new TextWriterTraceListener(logFile));
-            Trace.AutoFlush = true;
-            Trace.WriteLine("Starting Calculator Log");
-            Trace.WriteLine(string.Format("Started {0}", DateTime.Now.ToString()));
+            List<string> charList = new List<string>();
+            //input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+            foreach (var character in input)
+            {
+                charList.Add(character.ToString());
+            }
+            int marker = 0;
+            int markerB = 0;
+            for (int i = 0; i <= charList.Count(); i++)
+            {
+                List<string> tempList = new List<string> { charList[i], charList[i+1], charList[i+2], charList[i+3] };
+                bool isUnique = tempList.Distinct().Count() == tempList.Count();
+                if(isUnique)
+                {
+                    marker = i+4;
+                    break;
+                }
+            }
+
+            for (int i = 0; i <= charList.Count(); i++)
+            {
+                List<string> tempList = new List<string> { charList[i], charList[i + 1], charList[i + 2], charList[i + 3], charList[i + 4], charList[i + 5], charList[i + 6], charList[i + 7], charList[i + 8], charList[i + 9], charList[i + 10], charList[i + 11], charList[i + 12], charList[i + 13] };
+                bool isUnique = tempList.Distinct().Count() == tempList.Count();
+                if (isUnique)
+                {
+                    markerB = i + 14;
+                    break;
+                }
+            }
+
+
+
+
+            output = "Part A: " + marker;
+            output += "\nPart B: " + markerB;
+
+
         }
 
         public string output;
