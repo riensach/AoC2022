a042bc5b2af6e4f2bf6b9cee430febb850f560ba
Rien Sach <rien.sach@aptem.co.uk>
2022-12-16 17:21:11 +0000

    Day 13 done

--- solution/AoC2022Day13.cs
+++ solution/AoC2022Day13.cs
@@ -7,72 +7,13 @@ using Newtonsoft.Json.Linq;
 using System.Reflection;
 using System.Collections.Generic;
 using System.Collections;
-//using System.Text.Json;
-
 
 namespace AoC2022.solution
 {
-
         public class AoCDay13
         {
         public List<string> packets = new List<string>();
         public List<string> packetsOrdered = new List<string>();
-
-
-        public int comparison(JsonElement leftSide, JsonElement rightSide)
-        {
-            JsonElement newComp;
-            switch (leftSide.ValueKind, rightSide.ValueKind)
-            {
-                case (JsonValueKind.Null, not JsonValueKind.Null):
-                    return -1;
-                case (not JsonValueKind.Null, JsonValueKind.Null):
-                    return 1;
-                case (JsonValueKind.Number, JsonValueKind.Number):
-                    if(leftSide.GetInt32() < rightSide.GetInt32())
-                    {
-                        return -1;
-                    } else if (leftSide.GetInt32() > rightSide.GetInt32())
-                    {
-                        return 1;
-                    }
-                    return 0;
-                case (JsonValueKind.Number, JsonValueKind.Array):
-                    newComp = JsonSerializer.Deserialize<JsonElement>("[" + leftSide.GetInt32() + "]");
-                    return comparison(newComp, rightSide);
-                case (JsonValueKind.Array, JsonValueKind.Number):
-                    newComp = JsonSerializer.Deserialize<JsonElement>("[" + rightSide.GetInt32() + "]");
-                    return comparison(leftSide, newComp);
-                case (JsonValueKind.Array, JsonValueKind.Array):
-                    var leftArray = leftSide.EnumerateArray();
-                    var rightArray = rightSide.EnumerateArray();
-
-                    while (leftArray.MoveNext() && rightArray.MoveNext())
-                    {
-                        var leftArrayItem = leftArray.Current;
-                        var rightArrayItem = rightArray.Current;
-
-                        var compair = comparison(leftArrayItem, rightArrayItem);
-
-                        if (compair != 0)
-                        {
-                            return compair;
-                        }
-                    }
-
-                    var countItem = leftArray.Count() - rightArray.Count();
-
-                    return countItem switch
-                    {
-                        0 => 0,
-                        < 0 => -1,
-                        _ => 1
-                    };
-                default:
-                    return 0;
-            }
-        }
-
 
         public AoCDay13(int selectedPart, string input)
         {
@@ -141,22 +82,6 @@ namespace AoC2022.solution
                         packetsOrdered.RemoveAt(i);
                     }
                 }
-                for (int i = 0; i < packetsOrdered.Count - 1; i++)
-                {
-                    JsonElement packetComp1 = JsonSerializer.Deserialize<JsonElement>(packetsOrdered[i]);
-                    JsonElement packetComp2 = JsonSerializer.Deserialize<JsonElement>(packetsOrdered[i + 1]);
-
-                    int resultFound = comparison(packetComp1, packetComp2);
-
-                    if (resultFound >= 0)
-                    {
-                        correctOrder = false;
-                        packetsOrdered.Insert(i + 2, packetsOrdered[i]);
-                        packetsOrdered.RemoveAt(i);
-                    }
-                }
-
-
 
                 if (correctOrder == true)
                 {
@@ -164,13 +89,68 @@ namespace AoC2022.solution
                 }
             }
 
-
             int packet1 = packetsOrdered.FindIndex( x => x.Equals("[[2]]")) + 1;
             int packet2 = packetsOrdered.FindIndex( x => x.Equals("[[6]]")) + 1;
 
             int keyValue = packet1 * packet2;
 
             output += "\nPart B: " + keyValue + ". Index: "+ packet1+ " - "+ packet2;
+        }
+
+
+        public int comparison(JsonElement leftSide, JsonElement rightSide)
+        {
+            JsonElement newComp;
+            switch (leftSide.ValueKind, rightSide.ValueKind)
+            {
+                case (JsonValueKind.Null, not JsonValueKind.Null):
+                    return -1;
+                case (not JsonValueKind.Null, JsonValueKind.Null):
+                    return 1;
+                case (JsonValueKind.Number, JsonValueKind.Number):
+                    if (leftSide.GetInt32() < rightSide.GetInt32())
+                    {
+                        return -1;
+                    }
+                    else if (leftSide.GetInt32() > rightSide.GetInt32())
+                    {
+                        return 1;
+                    }
+                    return 0;
+                case (JsonValueKind.Number, JsonValueKind.Array):
+                    newComp = JsonSerializer.Deserialize<JsonElement>("[" + leftSide.GetInt32() + "]");
+                    return comparison(newComp, rightSide);
+                case (JsonValueKind.Array, JsonValueKind.Number):
+                    newComp = JsonSerializer.Deserialize<JsonElement>("[" + rightSide.GetInt32() + "]");
+                    return comparison(leftSide, newComp);
+                case (JsonValueKind.Array, JsonValueKind.Array):
+                    var leftArray = leftSide.EnumerateArray();
+                    var rightArray = rightSide.EnumerateArray();
+
+                    while (leftArray.MoveNext() && rightArray.MoveNext())
+                    {
+                        var leftArrayItem = leftArray.Current;
+                        var rightArrayItem = rightArray.Current;
+
+                        var compair = comparison(leftArrayItem, rightArrayItem);
+
+                        if (compair != 0)
+                        {
+                            return compair;
+                        }
+                    }
+
+                    var countItem = leftArray.Count() - rightArray.Count();
+
+                    return countItem switch
+                    {
+                        0 => 0,
+                        < 0 => -1,
+                        _ => 1
+                    };
+                default:
+                    return 0;
+            }
         }
 
         public string output;
