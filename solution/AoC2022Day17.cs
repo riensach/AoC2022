d130b121a19efde8b238d3ce92a4d5bd6bd7f4e8
Rien Sach <rien.sach@btinternet.com>
2022-12-18 21:57:33 +0000

    Updates

--- solution/AoC2022Day17.cs
+++ solution/AoC2022Day17.cs
@@ -11,38 +11,30 @@ namespace AoC2022.solution
         public AoCDay17(int selectedPart, string input)
         {
             char[] commands = input.ToCharArray();
-
-
             int arrayLength = 1400;
             int arrayWidth = 7;
             grid = new string[arrayLength, arrayWidth];
             createGrid(arrayLength, arrayWidth);
-
-
         }
         public string printGrid(int xSize, int ySize)
         {
             string output = "\nGrid:\n";
-
             for (int x = 0; x < xSize; x++)
             {
                 for (int y = 0; y < ySize; y++)
                 {
                     string toWrite = grid[x, y];
                     //System.Console.Write(toWrite);
-
                     output += "" + toWrite;
                 }
                 //System.Console.Write("\n");
                 output += "\n";
             }
-
             return output;
         }
 
         public void createGrid(int xSize, int ySize)
         {
-
             for (int x = 0; x < xSize; x++)
             {
                 for (int y = 0; y < ySize; y++)
@@ -50,9 +42,7 @@ namespace AoC2022.solution
                     grid[x, y] = ".";
                 }
             }
-
         }
-
         public string output;
     }
 }
