1cc0e262b2ad037e0cec1b36fb5f2fdda08748c2
Rien Sach <rien.sach@aptem.co.uk>
2022-12-20 12:38:05 +0000

    Did it!

--- solution/AoC2022Day17.cs
+++ solution/AoC2022Day17.cs
@@ -12,25 +12,45 @@ namespace AoC2022.solution
         public string[,] grid;
         public List<string[]> rockFormations = new List<string[]>();
         public int highestRock = 500000000;
+        public long partBRocks = 500000000;
+        int commandsSize;
+        char[] commands;
+        int arrayLength;
+        int arrayWidth = 7;
+        int currentHighestPlace;
         public AoCDay17(int selectedPart, string input)
         {
-            char[] commands = input.ToCharArray();
-            int commandsSize = commands.Length;
-            int arrayLength = 25000;
-            int arrayWidth = 7;
-            int currentHighestPlace = arrayLength;
+            commands = input.ToCharArray();
+            commandsSize = commands.Length;
+            createRockFormations();
+            solveRocks(45000, 2022);
+            int highRock = arrayLength - highestRock;
+            output += "Part A: Highest Rock: " + highRock;
+            highestRock = 500000000;
+            solveRocks(45000, 1000000000000);
+            output += "\nPart B: Highest Rock: " + partBRocks;
+
+        }
+
+        public void solveRocks(int spaceHeight, long totalRocks)
+        {
+
+            arrayLength = spaceHeight;
+            currentHighestPlace = arrayLength;
+            int findMatchLimit = 1000;
+            int lastHeight = arrayLength;
             List<int> foundPatternAt = new List<int>();
+            List<int> differences = new List<int>();
             grid = new string[arrayLength, arrayWidth];
             createGrid(arrayLength, arrayWidth);
-            createRockFormations();
             int listSize = rockFormations.Count;
 
             int commandIndex = 0;
-            for (int i = 0; i <= 4776; i++)
+            for (int i = 0; i < totalRocks; i++)
             {
                 int lowestY = 7;
                 int highestY = -1;
-                int currentRockIndex = (i < 5 ? i :i % listSize);
+                int currentRockIndex = (i < 5 ? i : i % listSize);
                 string[] rockFormation = rockFormations[currentRockIndex];
                 List<string> fallingRocks = new List<string>();
                 foreach (string coord in rockFormation)
@@ -54,8 +74,8 @@ namespace AoC2022.solution
                 bool foundRested = false;
                 bool foundRestedBeforeMove = false;
 
-                while(!rested)
-                {                                       
+                while (!rested)
+                {
                     // First we move with the jet
                     List<string> fallingRocksMoved = new List<string>();
                     List<string> fallingRocksBeforeMoved = fallingRocks.Select(i => new string(i)).ToList();
@@ -69,7 +89,7 @@ namespace AoC2022.solution
                         int y = Int32.Parse(parts[1]);
                         int currentCommandIndex = (commandIndex < commandsSize ? commandIndex : commandIndex % commandsSize);
                         char command = commands[currentCommandIndex];
-                        if (command=='<' && lowestY > 0)
+                        if (command == '<' && lowestY > 0)
                         {
                             // Push left
                             y = y - 1;
@@ -80,7 +100,8 @@ namespace AoC2022.solution
                                 // Can't move there!
                                 foundRestedBeforeMove = true;
                             }
-                        } else if (command == '>' && highestY < 6)
+                        }
+                        else if (command == '>' && highestY < 6)
                         {
                             // Push right
                             y = y + 1;
@@ -95,10 +116,11 @@ namespace AoC2022.solution
                         fallingRocksMoved.Add(x + "," + y);
                     }
                     commandIndex++;
-                    if(foundRestedBeforeMove)
+                    if (foundRestedBeforeMove)
                     {
                         fallingRocksMoved = fallingRocks.Select(i => new string(i)).ToList();
-                    } else
+                    }
+                    else
                     {
                         highestY = newHighY;
                         lowestY = newLowY;
@@ -128,16 +150,17 @@ namespace AoC2022.solution
                     }
                     fallingRocks = fallingRocksMovedDown.Select(i => new string(i)).ToList();
 
-                    if(foundRested)
+                    if (foundRested)
                     {
-                        if(foundRestedBeforeMove)
+                        if (foundRestedBeforeMove)
                         {
-                            fallingRocks = fallingRocksBeforeMoved.Select(i => new string(i)).ToList();                            
-                        } else
-                        {
-                            fallingRocks = fallingRocksMoved.Select(i => new string(i)).ToList(); 
+                            fallingRocks = fallingRocksBeforeMoved.Select(i => new string(i)).ToList();
                         }
-                        
+                        else
+                        {
+                            fallingRocks = fallingRocksMoved.Select(i => new string(i)).ToList();
+                        }
+
                         foreach (string rock in fallingRocks)
                         {
                             string[] parts = rock.Split(",");
@@ -149,172 +172,73 @@ namespace AoC2022.solution
                                 highestRock = x;
                                 currentHighestPlace = highestRock;
                             }
+                            
                         }
-                        rested = true;                      
-                    } 
-                }
 
-                int prevGapa = 0;
-                for (int x = 1; x < arrayLength; x++)
-                {
-                    List<int> mainRow = new List<int>();
-                    int rowCol1 = (grid[x, 0] == "#" ? 1 : 0);
-                    int rowCol2 = (grid[x, 1] == "#" ? 1 : 0);
-                    int rowCol3 = (grid[x, 2] == "#" ? 1 : 0);
-                    int rowCol4 = (grid[x, 3] == "#" ? 1 : 0);
-                    int rowCol5 = (grid[x, 4] == "#" ? 1 : 0);
-                    int rowCol6 = (grid[x, 5] == "#" ? 1 : 0);
-                    int rowCol7 = (grid[x, 6] == "#" ? 1 : 0);
-                    mainRow.Add(rowCol1);
-                    mainRow.Add(rowCol2);
-                    mainRow.Add(rowCol3);
-                    mainRow.Add(rowCol4);
-                    mainRow.Add(rowCol5);
-                    mainRow.Add(rowCol6);
-                    mainRow.Add(rowCol7);
-                    List<int> mainRowAbove = new List<int>();
-                    int rowColAbove1 = (grid[x - 1, 0] == "#" ? 1 : 0);
-                    int rowColAbove2 = (grid[x - 1, 1] == "#" ? 1 : 0);
-                    int rowColAbove3 = (grid[x - 1, 2] == "#" ? 1 : 0);
-                    int rowColAbove4 = (grid[x - 1, 3] == "#" ? 1 : 0);
-                    int rowColAbove5 = (grid[x - 1, 4] == "#" ? 1 : 0);
-                    int rowColAbove6 = (grid[x - 1, 5] == "#" ? 1 : 0);
-                    int rowColAbove7 = (grid[x - 1, 6] == "#" ? 1 : 0);
-                    mainRowAbove.Add(rowColAbove1);
-                    mainRowAbove.Add(rowColAbove2);
-                    mainRowAbove.Add(rowColAbove3);
-                    mainRowAbove.Add(rowColAbove4);
-                    mainRowAbove.Add(rowColAbove5);
-                    mainRowAbove.Add(rowColAbove6);
-                    mainRowAbove.Add(rowColAbove7);
-                    if (mainRow.Sum() == 7 && mainRowAbove.Sum() == 6 && !foundPatternAt.Contains(x))
-                    {
-                        foundPatternAt.Add(x);
-                        prevGapa = prevGapa - x;
-                        output += "\nPattern found at " + x + " with the row above: " + mainRowAbove.Sum() + " and gap from previous row: " + prevGapa + " and thew this many rocks: " + i;
-                        prevGapa = x;
+                        
+                        
+                        rested = true;
                     }
                 }
-            }
-            //output += printGrid(arrayLength, arrayWidth);
-            int highRock = arrayLength - highestRock;
-            output += "Part A: Highest Rock: " + highRock;
+                int diffToAdd = (lastHeight - currentHighestPlace);
+                differences.Add(diffToAdd);
+                lastHeight = currentHighestPlace;
 
-            int prevGap = 0;
-            for (int x = 1; x < arrayLength; x++)
-            {
-                List<int> mainRow = new List<int>();
-                int rowCol1 = (grid[x, 0] == "#" ? 1 : 0);
-                int rowCol2 = (grid[x, 1] == "#" ? 1 : 0);
-                int rowCol3 = (grid[x, 2] == "#" ? 1 : 0);
-                int rowCol4 = (grid[x, 3] == "#" ? 1 : 0);
-                int rowCol5 = (grid[x, 4] == "#" ? 1 : 0);
-                int rowCol6 = (grid[x, 5] == "#" ? 1 : 0);
-                int rowCol7 = (grid[x, 6] == "#" ? 1 : 0);
-                mainRow.Add(rowCol1);
-                mainRow.Add(rowCol2);
-                mainRow.Add(rowCol3);
-                mainRow.Add(rowCol4);
-                mainRow.Add(rowCol5);
-                mainRow.Add(rowCol6);
-                mainRow.Add(rowCol7);
-                List<int> mainRowAbove = new List<int>();
-                int rowColAbove1 = (grid[x - 1, 0] == "#" ? 1 : 0);
-                int rowColAbove2 = (grid[x - 1, 1] == "#" ? 1 : 0);
-                int rowColAbove3 = (grid[x - 1, 2] == "#" ? 1 : 0);
-                int rowColAbove4 = (grid[x - 1, 3] == "#" ? 1 : 0);
-                int rowColAbove5 = (grid[x - 1, 4] == "#" ? 1 : 0);
-                int rowColAbove6 = (grid[x - 1, 5] == "#" ? 1 : 0);
-                int rowColAbove7 = (grid[x - 1, 6] == "#" ? 1 : 0);
-                mainRowAbove.Add(rowColAbove1);
-                mainRowAbove.Add(rowColAbove2);
-                mainRowAbove.Add(rowColAbove3);
-                mainRowAbove.Add(rowColAbove4);
-                mainRowAbove.Add(rowColAbove5);
-                mainRowAbove.Add(rowColAbove6);
-                mainRowAbove.Add(rowColAbove7);
-                if (mainRow.Sum() == 7 && mainRowAbove.Sum() == 6)
+
+                // NOW WE LOOK FOR A PATTERN
+                if (i >= (2 * findMatchLimit))
                 {
-                    prevGap = prevGap - x;
-                    output += "\nPattern found at " + x + " with the row above: " + mainRowAbove.Sum() + " and gap from previous row: " + prevGap;
-                    prevGap = x;
+                    // Check for patterns?
+                    string differencesString = System.String.Join("-", differences.ToArray());
+
+
+                    List<int> differencesRecent = new List<int>();
+                    differences.GetRange((differences.Count() - findMatchLimit),(differences.Count() - (differences.Count() - findMatchLimit))).ForEach(h => differencesRecent.Add(h));
+                    string differencesRecentSearch = System.String.Join("", differencesRecent.ToArray());
+
+                    List<int> differencesPrevious = new List<int>();
+                    differences.GetRange(0, (differences.Count() - findMatchLimit)).ForEach(h => differencesPrevious.Add(h));
+                    string differencesPreviousSearch = System.String.Join("", differencesPrevious.ToArray());
+
+                    int foundRepeat = differencesPreviousSearch.LastIndexOf(differencesRecentSearch);
+
+                    if (foundRepeat >= 0)
+                    {
+                        
+                        int patternLength = i - findMatchLimit - foundRepeat + 1;
+                        if(patternLength > findMatchLimit)
+                        {
+                            findMatchLimit = patternLength;
+                            continue;
+                        }
+
+                        // We have a pattern, we now need to do some things
+                        // First, how high is the pattern?
+                        List<int> differencesPattern = new List<int>();
+                        differences.GetRange(foundRepeat, patternLength).ForEach(h => differencesPattern.Add(h));
+                        string differencesPatternString = System.String.Join("", differencesPattern.ToArray());
+
+                        long remainingSteps = totalRocks - foundRepeat;
+                        long multiple =  (long)Math.Floor(Convert.ToDouble(remainingSteps) / Convert.ToDouble(patternLength));
+                        int remainder = Convert.ToInt32(remainingSteps % patternLength);
+
+                        long int4 = differencesPattern.Sum(g => Convert.ToInt32(g));
+
+                        long newInt = differences.GetRange(0, foundRepeat).Sum(g => Convert.ToInt32(g));
+                        long newInt2 = multiple * int4;
+                        long newInt3 = differencesPattern.GetRange(0, remainder).Sum(g => Convert.ToInt32(g));
+                        partBRocks = newInt + newInt2 + newInt3;
+                        return;
+                    }
+
+                    if (i > 20000)
+                    {
+                        return; // Lets put this here to stop it going on forever
+                    }
                 }
+
             }
-
-
-            // PATTERN FOUND
-            // 6,952
-
-
-
-
-            // 319 rocks then pattern every 1740 rocks
-            // 319 rocks 474 height
-            // 2059 rocks = 3,155 height (2681)
-            // 3,799 = height = 5,836 (2681)
-            // 5,539 = height = 8,517 (2681)
-            // 7279 = height 11,198 (2681)
-
-            // So, after the first 319 rocks (474 height), each 2059 rocks adds on 2681 height
-            // so 1000000000000 % 2059 = 1296 remaining
-            // So 1000000000000 / 2059 = 485,672,656 * 2681 = 1,302,088,390,736 = total rocks from the main parts
-            // So 474 + 1,302,088,390,736 + what left from 977 more rocks (1,519) = 
-            // plus 977 more after 2059 = 1,519
-
-            // Submitted 1,302,088,392,729 but it's wrong
-
-            // 1,540,804,597,682
-
-
-
-            // 4776 rocks = 7355 - height of 5836 = 
-
-
-
-
-            // 319 rocks 474 height at the start from  with a maximum height of 479
-            // 2681 is a repeating pattern
-            // 3713 rocks = height of 5715
-            // 6394 rocks = height of 9832 (diff above 4117)
-            // 9075 rocks = height of 13973 (diff above 4141)
-            // 11756 rocks = height of 18110 (diff above 4137)
-
-
-
-
-            // 
-            // 3000 rocks = height of 4618
-            // 5681 rocks = height of 8751 (4133 more than below)
-            // 8362 rocks = height of 12888 (4137 more than below)
-            // 11043 rocks = height of 16994 (4106 more than below)
-
-
-            // 2701 rocks = hiehgt of 4136
-            // 5402 rocks = hiehgt of 8327 (4191 from above)
-            // 8103 rocks = hiehgt of 12470 (4143 from above)
-
-
-
-            // 4802 - 7483 - 10164 - 12845
-
-
-
-
-
-
-
-
-            // 3155 rocks to start gives a height of 5715
-            // So 3,239 inbetween gives a height of 4988
-            // Then pattern every 2681
-            // 3713 rocks = 2681 pattern + 474 to start pattern
-            // 1000000000000 = 169 + 3713 = 3882
-            // 372,995,150 times in the above number - 1
-            // 516572544
-
-
         }
-
 
         public string printGrid(int xSize, int ySize)
         {
