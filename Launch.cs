03de87807af1426d8a1d7631572e0df2b14e38ee
Rien Sach <rien.sach@aptem.co.uk>
2022-12-21 07:35:47 +0000

    Day 21

--- Launch.cs
+++ Launch.cs
@@ -16,7 +16,7 @@ namespace AdventofCode2022
             Console.WriteLine("------------------------\n");
 
 
-            int daySelection = 20;
+            int daySelection = 21;
             int partSelection = 1;
 
             DaySelection Advent = new DaySelection(daySelection, partSelection);
