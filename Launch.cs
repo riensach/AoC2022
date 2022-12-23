09b36a7bef5649805b205e2a6bb775dd1f3522e2
Rien Sach <rien.sach@aptem.co.uk>
2022-12-23 09:02:37 +0000

    Day 23 in da bag!

--- Launch.cs
+++ Launch.cs
@@ -16,7 +16,7 @@ namespace AdventofCode2022
             Console.WriteLine("------------------------\n");
 
 
-            int daySelection = 19;
+            int daySelection = 23;
             int partSelection = 1;
 
             DaySelection Advent = new DaySelection(daySelection, partSelection);
